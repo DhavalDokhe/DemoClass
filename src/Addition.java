6e5742f60bae49dc3a75817b48f4ff2325d6edea
DhavalDokhe <dhavaldokhe@gmail.com>
2021-11-15 13:52:52 +0530

    initial commit

--- src/Addition.java
+++ src/Addition.java
@@ -1,7 +1,7 @@
 public class Addition {
     public static void main(String[] args) {
-        int a=10;
-        int b=20;
-        System.out.println(a+b);
+        int a=20;
+        int b=3;
+        System.out.println(a/b);
     }
 }
